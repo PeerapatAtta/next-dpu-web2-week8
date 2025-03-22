b125efdfc81c52fcf9579dfdd13ff8bf3b927603
OFF <peerapat13th@gmail.com>
2025-03-22 19:23:52 +0700

    Updated

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h2>ยินดีต้อนรับเข้าสู่เว็บไซต์ของเรา</h2>
+      <h2>ยินดีต้อนรับเข้าสู่เว็บไซต์ของเรา Updated</h2>
       <Link href="/about">ไปยังหน้าเกี่ยวกับเรา</Link>
     </div>
   );
